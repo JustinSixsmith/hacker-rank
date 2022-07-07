378518ebb20f85d6a7ad11462e625b6f238aa3fb
Justin Sixsmith <justinsixsmith@gmail.com>
2022-07-07 13:33:48 -0500

    solves for multiplier

---
+++ src/Multiples.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Multiples {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int multiplier = 1;
+        int result = 0;
+
+        for (int i = 0; i < 10; i++) {
+            result = N * multiplier;
+            System.out.println(N + " x " + multiplier + " = " + result);
+            multiplier++;
+        }
+
+        bufferedReader.close();
+    }
+}
