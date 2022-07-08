89c2a9ab41fc2d5c3b21334c58ae3b465ce735b9
Justin Sixsmith <justinsixsmith@gmail.com>
2022-07-08 17:29:27 -0500

    solves for java end-of-file

---
+++ src/JavaEOF.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class JavaEOF {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int count = 1;
+        while (scanner.hasNextLine()) {
+            System.out.println(count + " " + scanner.nextLine());
+            count++;
+        }
+    }
+}
