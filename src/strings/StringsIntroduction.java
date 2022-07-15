3b3b3ae35cb15861980b88aa7832f7b2b6f6b209
Justin Sixsmith <justinsixsmith@gmail.com>
2022-07-14 20:13:51 -0500

    refactors to pass all tests

--- src/strings/StringsIntroduction.java
+++ src/strings/StringsIntroduction.java
@@ -7,25 +7,11 @@ public class StringsIntroduction {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scan = new Scanner(System.in);
-        String A = scan.nextLine();
-        String B = scan.nextLine();
-        String upperA = A.toUpperCase();
-        String upperB = B.toUpperCase();
-        char firstA = upperA.charAt(0);
-        char firstB = upperB.charAt(0);
-        String remA = A.substring(1);
-        String remB = B.substring(1);
-        int sumLength = A.length() + B.length();
-        String quote = firstA + remA + " " + firstB + remB;
-        boolean lexi = firstB <= firstA;
-
-        System.out.println(sumLength);
-        if (lexi) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
-        System.out.println(quote);
+        String A = scan.next();
+        String B = scan.next();
+        System.out.println(A.length() + B.length());
+        System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
+        System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1) + " " + B.substring(0, 1).toUpperCase() + B.substring(1));
 
         scan.close();
     }
