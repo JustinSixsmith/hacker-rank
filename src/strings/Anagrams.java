dad405cec78c2cc219b203d508de3811b5f12a71
Justin Sixsmith <justinsixsmith@gmail.com>
2022-07-18 20:15:46 -0500

    creats class for Anagrams exercise

---
+++ src/strings/Anagrams.java
@@ -0,0 +1,21 @@
+package strings;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Anagrams {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        String a = scan.nextLine().toLowerCase();
+        String b = scan.nextLine().toLowerCase();
+
+        for (int i = 0; i < a.length(); i++) {
+
+
+        }
+
+
+
+    }
+}
