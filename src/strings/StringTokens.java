3884c8dd6b4a8d97f29c098b0d94d25c9ce2277e
Justin Sixsmith <justinsixsmith@gmail.com>
2022-07-20 14:52:21 -0500

    solves for string tokens

---
+++ src/strings/StringTokens.java
@@ -0,0 +1,21 @@
+package strings;
+
+import java.util.Scanner;
+
+public class StringTokens {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine().trim();
+
+        if (s.length() > 0) {
+            String[] tokens = s.split("[ !,?._'@]+");
+            System.out.println(tokens.length);
+            for (String token :
+                    tokens) {
+                System.out.println(token);
+            }
+        } else {
+            System.out.println(0);
+        }
+    }
+}
