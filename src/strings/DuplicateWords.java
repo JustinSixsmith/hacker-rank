1a50397dac337bbd04366b22a63c8632feb0f808
Justin Sixsmith <justinsixsmith@gmail.com>
2022-07-24 11:46:24 -0500

    refactors to work with java 7

--- src/strings/DuplicateWords.java
+++ src/strings/DuplicateWords.java
@@ -5,24 +5,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DuplicateWords {
+
     public static void main(String[] args) {
 
-        String regex = "\\b(\\w+)(\\W+\\1\\b)+";
-        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        String regex = "(\\b\\w+)(\\s\\1\\b)+";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE + Pattern.MULTILINE);
 
-        Scanner scanner = new Scanner(System.in);
-        int numTime = Integer.parseInt(scanner.nextLine());
+        Scanner in = new Scanner(System.in);
+        int numSentences = Integer.parseInt(in.nextLine());
 
-        while (numTime-- > 0) {
-            String input = scanner.nextLine();
+        while (numSentences-- > 0) {
+            String input = in.nextLine();
 
-            Matcher matcher = pattern.matcher(input);
+            Matcher m = p.matcher(input);
 
-            while (matcher.find()) {
-                input = input.replaceAll(matcher.group(), matcher.group(1));
+            // Check for subsequences of input that match the compiled pattern
+            while (m.find()) {
+                input = input.replaceAll(m.group(), m.group(1));
             }
+
+            // Prints the modified sentence.
             System.out.println(input);
         }
-        scanner.close();
+
+        in.close();
     }
 }
