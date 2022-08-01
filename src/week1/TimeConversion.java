3f6b6ff390464b1a5ef436cb45bafbcbd2385c11
Justin Sixsmith <justinsixsmith@gmail.com>
2022-07-31 20:06:53 -0500

    adds code for converting to military time:

--- src/week1/TimeConversion.java
+++ src/week1/TimeConversion.java
@@ -1,14 +1,10 @@
 package week1;
 
 import java.io.*;
-        import java.math.*;
-        import java.security.*;
-        import java.text.*;
-        import java.util.*;
-        import java.util.concurrent.*;
-        import java.util.regex.*;
 
-class Result {
+import static java.lang.Integer.parseInt;
+
+class TimeResult {
 
     /*
      * Complete the 'timeConversion' function below.
@@ -19,10 +15,23 @@ class Result {
 
     public static String timeConversion(String s) {
         // Write your code here
+        String time = s.substring(8);
 
+        String hours = s.substring(0, 2);
+        String minutes = s.substring(3, 5);
+        String seconds = s.substring(6, 8);
+
+        if (time.equals("AM") && hours.equals("12")) {
+            hours = "00";
+        } else if (time.equals("PM") && hours.equals("12")) {
+            hours = "12";
+        } else if (time.equals("PM")) {
+            hours = String.valueOf(Integer.parseInt(hours) + 12);
+        }
+        return hours + ":" + minutes + ":" + seconds;
     }
-
 }
+
 
 public class TimeConversion {
     public static void main(String[] args) throws IOException {
@@ -31,7 +40,7 @@ public class TimeConversion {
 
         String s = bufferedReader.readLine();
 
-        String result = Result.timeConversion(s);
+        String result = TimeResult.timeConversion(s);
 
         bufferedWriter.write(result);
         bufferedWriter.newLine();
