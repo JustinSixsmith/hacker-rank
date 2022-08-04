10cf31c0c4fa2e4871c1b7599ce9c7d59c27f476
Justin Sixsmith <justinsixsmith@gmail.com>
2022-08-03 20:19:29 -0500

    changes length of binary string to 32 digits long

--- src/week1/FlippingBits.java
+++ src/week1/FlippingBits.java
@@ -21,32 +21,44 @@ class FlipResult {
 
     public static long flippingBits(long n) {
         // Write your code here
-        String tempBinary = Long.toBinaryString(n);
-        String binary = tempBinary.replaceAll("1", "0").replaceAll("0", "1");
-        long flippedBinary = parseLong(binary);
-        return Long.parseLong(String.valueOf(flippedBinary));
+        String oldBinary = Long.toBinaryString(n);
+        while(oldBinary.length() < 32) {
+            oldBinary = "0" + oldBinary;
+        }
+        StringBuilder newBinary = new StringBuilder();
+        for (int i = 0; i <oldBinary.length(); i++) {
+            if (oldBinary.charAt(i) == '1') {
+                newBinary.append("0");
+            } else if (oldBinary.charAt(i) == '0') {
+                newBinary.append("1");
+            }
+        }
+        return Long.parseLong(String.valueOf(newBinary), 2);
     }
-
 }
 
 public class FlippingBits {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        System.out.println(FlipResult.flippingBits(2147483647));
+        System.out.println(FlipResult.flippingBits(1));
+        System.out.println(FlipResult.flippingBits(0));
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//
+//        int q = Integer.parseInt(bufferedReader.readLine().trim());
+//
+//        for (int qItr = 0; qItr < q; qItr++) {
+//            long n = parseLong(bufferedReader.readLine().trim());
+//
+//            long result = FlipResult.flippingBits(n);
+//
+//            bufferedWriter.write(String.valueOf(result));
+//            bufferedWriter.newLine();
+//        }
+//
+//        bufferedReader.close();
+//        bufferedWriter.close();
 
-        int q = Integer.parseInt(bufferedReader.readLine().trim());
-
-        for (int qItr = 0; qItr < q; qItr++) {
-            long n = parseLong(bufferedReader.readLine().trim());
-
-            long result = FlipResult.flippingBits(n);
-
-            bufferedWriter.write(String.valueOf(result));
-            bufferedWriter.newLine();
-        }
-
-        bufferedReader.close();
-        bufferedWriter.close();
     }
 }
 
