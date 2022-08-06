a4a23e11ad808b249f196d2d23b9d400dbfd6b3f
Justin Sixsmith <justinsixsmith@gmail.com>
2022-08-06 12:55:28 -0500

    refactors into single line statement

--- src/week1/Pangrams.java
+++ src/week1/Pangrams.java
@@ -14,41 +14,25 @@ class PangramResult {
 
     public static String pangrams(String s) {
         // Write your code here
-        List<Character> alphabet = new ArrayList<>();
-        int flag = 0;
-        for (char ch = 'a'; ch <= 'z'; ++ch) {
-            alphabet.add(ch);
-        }
-        for (Character ch :
-                alphabet) {
-            if (!s.contains(String.valueOf(ch))) {
-                flag = 1;
-            }
-        }
-        if (flag == 0) {
-            return "pangram";
-        } else {
-            return "not pangram";
-        }
+        return s.toLowerCase(Locale.ROOT).replace(" ", "").chars().distinct().count() == 26 ? "pangram" : "not pangram";
     }
 
 }
 
 public class Pangrams {
     public static void main(String[] args) throws IOException {
-        System.out.println(PangramResult.pangrams("the quick brown fox jumped over the lazy dog"));
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-//
-//        String s = bufferedReader.readLine();
-//
-//        String result = PangramResult.pangrams(s);
-//
-//        bufferedWriter.write(result);
-//        bufferedWriter.newLine();
-//
-//        bufferedReader.close();
-//        bufferedWriter.close();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = PangramResult.pangrams(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
 
