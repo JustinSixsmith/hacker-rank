58075fa2d966c3e9456c1024857b4a83174f50a3
Justin Sixsmith <justinsixsmith@gmail.com>
2022-08-10 13:25:51 -0500

    solves for permuting two arrays

--- src/week1/PermutingTwoArrays.java
+++ src/week1/PermutingTwoArrays.java
@@ -24,9 +24,16 @@ class PermutingResult {
 
     public static String twoArrays(int k, List<Integer> A, List<Integer> B) {
         // Write your code here
+        Collections.sort(A);
+        B.sort(Collections.reverseOrder());
 
+        for (int i = 0; i < A.size(); i++) {
+            if (A.get(i) + B.get(i) < k) {
+                return "NO";
+            }
+        }
+        return "YES";
     }
-
 }
 
 public class PermutingTwoArrays {
