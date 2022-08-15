b646f022cd8fa6d6f612d0828e96bc40325cc60c
Justin Sixsmith <justinsixsmith@gmail.com>
2022-08-15 11:17:43 -0500

    changes class name

--- src/week1/XORStrings2.java
+++ src/week1/XORStrings2.java
@@ -6,7 +6,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class XORStrings2 {
 
     public static String stringsXOR(String s, String t) {
         StringBuilder res = new StringBuilder(new String(""));
