f2f6b0a2f7b6c770f7d33c68670bcf2f259a592a
Justin Sixsmith <justinsixsmith@gmail.com>
2022-08-22 20:33:11 -0500

    sets up class to solve for Tower Breakers

--- src/week2/TowerBreakers.java
+++ src/week2/TowerBreakers.java
@@ -9,47 +9,50 @@ import java.util.concurrent.*;
 import java.util.function.*;
 import java.util.regex.*;
 import java.util.stream.*;
-
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Result {
 
     /*
-     * Complete the 'pageCount' function below.
+     * Complete the 'towerBreakers' function below.
      *
      * The function is expected to return an INTEGER.
      * The function accepts following parameters:
      *  1. INTEGER n
-     *  2. INTEGER p
+     *  2. INTEGER m
      */
 
-    public static int pageCount(int n, int p) {
+    public static int towerBreakers(int n, int m) {
         // Write your code here
-        int midPage = (n / 2);
-        if (p > midPage) {
-            if (n % 2 == 0) {
-                n += 1;
-            }
-            return (n - p) / 2;
-        }
-        return p / 2;
+
     }
+
 }
 
-public class DrawingBook {
+public class TowerBreakers {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
 
-        int p = Integer.parseInt(bufferedReader.readLine().trim());
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
-        int result = Result.pageCount(n, p);
+                int n = Integer.parseInt(firstMultipleInput[0]);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
+                int m = Integer.parseInt(firstMultipleInput[1]);
+
+                int result = Result.towerBreakers(n, m);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
 
         bufferedReader.close();
         bufferedWriter.close();
