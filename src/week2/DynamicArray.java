4112a814d4d2be060e0dc4637ae1a1cd5d735bc9
Justin Sixsmith <justinsixsmith@gmail.com>
2022-08-29 09:58:39 -0500

    creates two-dimensional array of empty arrays, last answer, and answer array

---
+++ src/week2/DynamicArray.java
@@ -0,0 +1,82 @@
+package week2;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class DynamicResult {
+
+    /*
+     * Complete the 'dynamicArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
+        // Write your code here
+
+        // Declare a two-dimensional array of empty arrays
+        List<ArrayList<Integer>> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            arr.add(new ArrayList<Integer>());
+        }
+
+        // Declare an integer and initialize it to zero
+        int lastAnswer = 0;
+
+        // Declare an answer array
+        List<Integer> answerArray = new ArrayList<>();
+
+        for (List<Integer> query :
+                queries) {
+            
+        }
+
+    }
+
+}
+
+public class DynamicArray {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int q = Integer.parseInt(firstMultipleInput[1]);
+
+        List<List<Integer>> queries = new ArrayList<>();
+
+        IntStream.range(0, q).forEach(i -> {
+            try {
+                queries.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        List<Integer> result = DynamicResult.dynamicArray(n, queries);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
