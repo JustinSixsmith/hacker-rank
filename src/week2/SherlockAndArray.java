a758782c0e643a02b9849b6f37ed01b368122067
Justin Sixsmith <justinsixsmith@gmail.com>
2022-09-02 09:37:29 -0500

    moves where left and right are initiated inside the loop

--- src/week2/SherlockAndArray.java
+++ src/week2/SherlockAndArray.java
@@ -20,12 +20,12 @@ class SherlockResult {
     public static String balancedSums(List<Integer> arr) {
         // Write your code here
         String answer = "NO";
-        int leftSum = 0;
-        int rightSum = 0;
+
 
         // Start with the element at index 1, end with element in second to last position
         for (int i = 1; i < arr.size() - 1; i++) {
-
+            int leftSum = 0;
+            int rightSum = 0;
             // Add up left side
             for (int j = 0; j < i; j++) {
                 leftSum += arr.get(j);
@@ -34,7 +34,8 @@ class SherlockResult {
             for (int j = i + 1; j < arr.size(); j++) {
                 rightSum += arr.get(j);
             }
-
+            System.out.println(leftSum);
+            System.out.println(rightSum);
             // Compare sides
             if (leftSum == rightSum) {
                 answer = "YES";
@@ -46,31 +47,35 @@ class SherlockResult {
 
 public class SherlockAndArray {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        List<Integer> arr = new ArrayList<>(Arrays.asList(1, 2, 3, 3));
+        System.out.println(SherlockResult.balancedSums(arr));
 
-        int T = Integer.parseInt(bufferedReader.readLine().trim());
 
-        for (int TItr = 0; TItr < T; TItr++) {
-            int n = Integer.parseInt(bufferedReader.readLine().trim());
-
-            String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-            List<Integer> arr = new ArrayList<>();
-
-            for (int i = 0; i < n; i++) {
-                int arrItem = Integer.parseInt(arrTemp[i]);
-                arr.add(arrItem);
-            }
-
-            String result = SherlockResult.balancedSums(arr);
-
-            bufferedWriter.write(result);
-            bufferedWriter.newLine();
-        }
-
-        bufferedReader.close();
-        bufferedWriter.close();
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//
+//        int T = Integer.parseInt(bufferedReader.readLine().trim());
+//
+//        for (int TItr = 0; TItr < T; TItr++) {
+//            int n = Integer.parseInt(bufferedReader.readLine().trim());
+//
+//            String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+//
+//            List<Integer> arr = new ArrayList<>();
+//
+//            for (int i = 0; i < n; i++) {
+//                int arrItem = Integer.parseInt(arrTemp[i]);
+//                arr.add(arrItem);
+//            }
+//
+//            String result = SherlockResult.balancedSums(arr);
+//
+//            bufferedWriter.write(result);
+//            bufferedWriter.newLine();
+//        }
+//
+//        bufferedReader.close();
+//        bufferedWriter.close();
     }
 }
 
