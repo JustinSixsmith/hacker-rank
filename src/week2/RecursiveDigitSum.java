29eee50760d6418290992f12662f2dd08055638a
Justin Sixsmith <justinsixsmith@gmail.com>
2022-09-05 18:29:34 -0500

    gets half the test cases

--- src/week2/RecursiveDigitSum.java
+++ src/week2/RecursiveDigitSum.java
@@ -26,21 +26,22 @@ class RecursiveResult {
             p += n;
             k--;
         }
-        System.out.println(p);
 
-        int digitSum = 0;
-        for (int i = 0; i < p.length(); i++) {
-            int digit = Integer.parseInt(String.valueOf(p.charAt(i)));
-            digitSum += digit;
+        while (p.length() > 1) {
+            int digitSum = 0;
+            for (int i = 0; i < p.length(); i++) {
+                int digit = Integer.parseInt(String.valueOf(p.charAt(i)));
+                digitSum += digit;
+            }
+            p = String.valueOf(digitSum);
         }
-        return digitSum;
+        return Integer.parseInt(p);
     }
-
 }
 
 public class RecursiveDigitSum {
     public static void main(String[] args) throws IOException {
-        System.out.println(RecursiveResult.superDigit("2", 21));
+        System.out.println(RecursiveResult.superDigit("2", 13));
 
 //        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 //        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
