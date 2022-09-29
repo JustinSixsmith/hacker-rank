f1776d3c172ea592aa5c3932720d190cc82efa77
Justin Sixsmith <justinsixsmith@gmail.com>
2022-09-29 11:57:43 -0500

    first pass at solving new years chaos

--- src/week3/NewYearsChaos.java
+++ src/week3/NewYearsChaos.java
@@ -9,10 +9,11 @@ import java.util.concurrent.*;
 import java.util.function.*;
 import java.util.regex.*;
 import java.util.stream.*;
+
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-class NewYearsResult {
+class NewYearResult {
 
     /*
      * Complete the 'minimumBribes' function below.
@@ -22,32 +23,53 @@ class NewYearsResult {
 
     public static void minimumBribes(List<Integer> q) {
         // Write your code here
+        int totalBribes = 0;
 
+        //Loop through list and compare value to its index + 1
+        for (int i = 0; i < q.size(); i++) {
+
+            if (q.get(i) < (i + 1) + 2) {
+                int bribes = q.get(i) - (i + 1);
+                totalBribes += bribes;
+
+            } else {
+                System.out.println("too chaotic");
+
+            }
+            System.out.println(totalBribes);
+        }
     }
 
 }
 
 public class NewYearsChaos {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int t = Integer.parseInt(bufferedReader.readLine().trim());
+        List<Integer> queue1 = new ArrayList<>(Arrays.asList(2, 1, 5, 3, 4));
+        List<Integer> queue2 = new ArrayList<>(Arrays.asList(2, 5, 3, 1, 4));
 
-        IntStream.range(0, t).forEach(tItr -> {
-            try {
-                int n = Integer.parseInt(bufferedReader.readLine().trim());
+//        NewYearResult.minimumBribes(queue1);
+        NewYearResult.minimumBribes(queue2);
 
-                List<Integer> q = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                        .map(Integer::parseInt)
-                        .collect(toList());
-
-                NewYearsResult.minimumBribes(q);
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        });
-
-        bufferedReader.close();
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int t = Integer.parseInt(bufferedReader.readLine().trim());
+//
+//        IntStream.range(0, t).forEach(tItr -> {
+//            try {
+//                int n = Integer.parseInt(bufferedReader.readLine().trim());
+//
+//                List<Integer> q = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+//                        .map(Integer::parseInt)
+//                        .collect(toList());
+//
+//                NewYearResult.minimumBribes(q);
+//            } catch (IOException ex) {
+//                throw new RuntimeException(ex);
+//            }
+//        });
+//
+//        bufferedReader.close();
     }
 }
 
