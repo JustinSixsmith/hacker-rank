9fd2da920c5a9392f649423c00d86eaa6ed1d916
Justin Sixsmith <justinsixsmith@gmail.com>
2022-10-06 21:51:54 -0500

    refactors using a comparator inside a sort

--- src/challenges/BigDecimal.java
+++ src/challenges/BigDecimal.java
@@ -14,7 +14,16 @@ class Solution{
         scanner.close();
 
         //Write your code here
-        Arrays.sort(s, Collections.reverseOrder(Comparator.comparing(BigDecimal::new)));
+//        Arrays.sort(s, Collections.reverseOrder(Comparator.comparing(BigDecimal::new)));
+
+        Arrays.sort(s, 0, n, Collections.reverseOrder(new Comparator<String>() {
+                    @Override
+                    public int compare(String o1, String o2) {
+                        BigDecimal a = new BigDecimal(o1);
+                        BigDecimal b = new BigDecimal(o2);
+                        return a.compareTo(b);
+                    }
+                }));
 
         // Output
         for (int i = 0; i < n; i++) {
