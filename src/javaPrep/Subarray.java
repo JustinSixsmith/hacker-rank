1eed68e53cd2e7669ccf244195471aac465a9617
Justin Sixsmith <justinsixsmith@gmail.com>
2022-10-13 21:06:33 -0500

    sets up class and takes in array size and number values for array

---
+++ src/javaPrep/Subarray.java
@@ -0,0 +1,15 @@
+package javaPrep;
+
+import java.io.*;
+import java.util.*;
+
+public class Subarray {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int arrayLength = scanner.nextInt();
+        String nums = scanner.nextLine();
+        String[] numArray = nums.split(" ");
+    }
+}
