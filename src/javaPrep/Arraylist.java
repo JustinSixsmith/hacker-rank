295657a1e9e3deda0b46a01b5c3afc9d413e8c3b
Justin Sixsmith <justinsixsmith@gmail.com>
2022-10-14 12:26:40 -0500

    prints all but last query

--- src/javaPrep/Arraylist.java
+++ src/javaPrep/Arraylist.java
@@ -1,6 +1,5 @@
 package javaPrep;
 
-import java.io.*;
 import java.util.*;
 
 public class Arraylist {
@@ -8,11 +7,38 @@ public class Arraylist {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
+        // Take in number of lines
+        int lines = sc.nextInt();
 
+        // Create an array list of int arrays
+        ArrayList<ArrayList<Integer>> numArrays = new ArrayList<>();
+
+        for (int i = 0; i < lines; i++) {
+            // Create an array of ints for each line input
+            ArrayList<Integer> numArray = new ArrayList<>();
+            int entries = sc.nextInt();
+            for (int j = 0; j < entries; j++) {
+                numArray.add(sc.nextInt());
+            }
+            numArrays.add(numArray);
+        }
+        System.out.println(numArrays);
+
+        int queries = sc.nextInt();
+        for (int i = 0; i < queries; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            System.out.println(i);
+            findNum(numArrays, x, y);
+        }
         sc.close();
     }
 
-    public static int findNum(ArrayList<Integer> arr) {
-
+    public static void findNum(ArrayList<ArrayList<Integer>> arr, int x, int y) {
+        try {
+            System.out.println(arr.get(x - 1).get(y - 1));
+        } catch (Exception e) {
+            System.out.println("ERROR!");
+        }
     }
 }
