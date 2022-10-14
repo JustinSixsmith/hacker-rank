d24d63d33c3d4e8d98fef12c919f18729e03b14e
Justin Sixsmith <justinsixsmith@gmail.com>
2022-10-14 11:44:12 -0500

    sets up class to solve for ArrayList

---
+++ src/javaPrep/Arraylist.java
@@ -0,0 +1,18 @@
+package javaPrep;
+
+import java.io.*;
+import java.util.*;
+
+public class Arraylist {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+
+    public static int findNum(ArrayList<Integer> arr) {
+
+    }
+}
