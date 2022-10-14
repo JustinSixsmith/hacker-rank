d703bb9998e66b86e60bad30fa7f89165a7e17ca
Justin Sixsmith <justinsixsmith@gmail.com>
2022-10-13 23:07:58 -0500

    solves for subarray

--- src/javaPrep/Subarray.java
+++ src/javaPrep/Subarray.java
@@ -6,16 +6,26 @@ import java.util.*;
 public class Subarray {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = scanner.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int[] arr = new int[length];
+        for (int i = 0; i < arr.length; i++){
+            arr[i]= sc.nextInt();
         }
-        scanner.close();
+        sc.close();
+        System.out.println(negativeSubarrays(arr));
+    }
+    private static int negativeSubarrays(int[] arr) {
         int count = 0;
-
-
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                if (sum < 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
