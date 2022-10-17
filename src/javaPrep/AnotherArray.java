a3fa2270860fd74b8d979c5ab8e1bc8cab0c45cf
Justin Sixsmith <justinsixsmith@gmail.com>
2022-10-17 11:57:26 -0500

    passes one test case

--- src/javaPrep/AnotherArray.java
+++ src/javaPrep/AnotherArray.java
@@ -6,6 +6,23 @@ public class AnotherArray {
 
     public static boolean canWin(int leap, int[] game) {
         // Return true if you can win the game; otherwise, return false.
+//        System.out.println(Arrays.toString(Arrays.stream(game).toArray()));
+        boolean win = false;
+        int pos = 0;
+        for (int i = 0; i < game.length; i++) {
+            if (game[i] == 0) {
+                pos++;
+            } else if (i + leap <= game.length - 1) {
+                if (game[i + leap] == 0)
+                    pos = game[i + leap];
+            }
+        }
+
+        // Check if able to leap to end
+        if (pos == game.length || pos + leap >= game.length) {
+            win = true;
+        }
+        return win;
     }
 
     public static void main(String[] args) {
@@ -20,7 +37,7 @@ public class AnotherArray {
                 game[i] = scan.nextInt();
             }
 
-            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+            System.out.println((canWin(leap, game)) ? "YES" : "NO");
         }
         scan.close();
     }
