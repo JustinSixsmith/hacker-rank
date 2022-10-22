93fb2f4f34bc24cb4bec0a5122fcb6cc4957fc83
Justin Sixsmith <justinsixsmith@gmail.com>
2022-10-22 08:11:05 -0500

    adjusts spacing

--- src/javaPrep/List.java
+++ src/javaPrep/List.java
@@ -10,13 +10,13 @@ public class List {
         Scanner sc = new Scanner(System.in);
         int listSize = sc.nextInt();
         ArrayList<Integer> list = new ArrayList<>();
-        for(int i = 0; i < listSize; i++){
+        for (int i = 0; i < listSize; i++) {
             list.add(sc.nextInt());
         }
 
-        int queries= sc.nextInt();
-        for(int i=0; i < queries; i++) {
-            String query= sc.next();
+        int queries = sc.nextInt();
+        for (int i = 0; i < queries; i++) {
+            String query = sc.next();
             if (query.equals("Insert")) {
                 list.add(sc.nextInt(), sc.nextInt());
             } else if (query.equals("Delete")) {
@@ -24,8 +24,8 @@ public class List {
             }
         }
 
-        for(Integer i: list){
-            System.out.printf("%d ",i);
+        for (Integer num: list) {
+            System.out.printf("%d ", num);
         }
     }
 }
