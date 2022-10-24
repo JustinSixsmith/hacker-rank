5c72b4c0267fc09ecc129bdc6f757cce79e0e2c6
Justin Sixsmith <justinsixsmith@gmail.com>
2022-10-24 10:12:40 -0500

    sets up class to solve for generics

---
+++ src/javaPrep/Generics.java
@@ -0,0 +1,30 @@
+package javaPrep;
+
+import java.io.*;
+import java.util.*;
+
+public class Generics {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int intLength = sc.nextInt();
+        int[] numbers = new int[intLength];
+        for (int i = 0; i < intLength; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        int wordLength = sc.nextInt();
+        String[] words = new String[wordLength];
+        for (int i = 0; i < wordLength; i++) {
+            words[i] = sc.nextLine();
+        }
+
+        printArray(numbers, words);
+        sc.close();
+    }
+
+    public static void printArray(int[] numbers, String[] words) {
+
+    }
+}
