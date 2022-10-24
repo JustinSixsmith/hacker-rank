9e4c783dec496440a3f061a45a60510d79825292
Justin Sixsmith <justinsixsmith@gmail.com>
2022-10-24 10:33:30 -0500

    refactors to create array manually

--- src/javaPrep/Generics.java
+++ src/javaPrep/Generics.java
@@ -7,29 +7,13 @@ public class Generics {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner sc = new Scanner(System.in);
-        int intLength = sc.nextInt();
-        int[] numbers = new int[intLength];
-        for (int i = 0; i < intLength; i++) {
-            numbers[i] = sc.nextInt();
-        }
-
-        int wordLength = sc.nextInt();
-        String[] words = new String[wordLength];
-        for (int i = 0; i < wordLength; i++) {
-            words[i] = sc.nextLine();
-        }
-
-        printArray(numbers, words);
-        sc.close();
+        Object[] arr = {1, 2, 3, "Hello", "World"};
+        printArray(arr);
     }
 
-    public static void printArray(int[] numbers, String[] words) {
-        for (int number : numbers) {
-            System.out.println(number);
-        }
-        for (String word : words) {
-            System.out.println(word);
+    private static void printArray(Object[] arr) {
+        for (Object obj : arr) {
+            System.out.println(obj);
         }
     }
 }
