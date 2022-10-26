d3d24dc5867fc562f3bacc4e605cf2cc10318a1a
Justin Sixsmith <justinsixsmith@gmail.com>
2022-10-26 09:45:09 -0500

    fixes object type in 2D Array

--- src/javaPrep/JavaList.java
+++ src/javaPrep/JavaList.java
@@ -1,9 +1,8 @@
 package javaPrep;
 
-import java.io.*;
 import java.util.*;
 
-public class List {
+public class JavaList {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
