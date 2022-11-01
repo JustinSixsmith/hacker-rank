8c8f26b5ad79e154da9337687bf0cc67fe4772d5
Justin Sixsmith <justinsixsmith@gmail.com>
2022-11-01 10:13:42 -0500

    renames comparator class to avoid conflict

--- src/javaPrep/JavaComparator.java
+++ src/javaPrep/JavaComparator.java
@@ -3,28 +3,28 @@ package javaPrep;
 import java.util.*;
 
 // Write your Checker class here
-class Checker implements Comparator<Player>{
+class Checker implements Comparator<Player> {
     @Override
-    public int compare(Player a, Player b){
-        if(b.score == a.score){
+    public int compare(Player a, Player b) {
+        if (b.score == a.score) {
             return a.name.compareTo(b.name);
-        } else if (b.score > a.score){
+        } else if (b.score > a.score) {
             return 1;
         } else return -1;
     }
 }
 
-class Player{
+class Player {
     String name;
     int score;
 
-    Player(String name, int score){
+    Player(String name, int score) {
         this.name = name;
         this.score = score;
     }
 }
 
-abstract class Comparator {
+abstract class JavaComparator {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -33,13 +33,13 @@ abstract class Comparator {
         Player[] player = new Player[n];
         Checker checker = new Checker();
 
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             player[i] = new Player(scan.next(), scan.nextInt());
         }
         scan.close();
 
         Arrays.sort(player, checker);
-        for(int i = 0; i < player.length; i++){
+        for (int i = 0; i < player.length; i++) {
             System.out.printf("%s %s\n", player[i].name, player[i].score);
         }
     }
