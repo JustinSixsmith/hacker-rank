0b1407cfa8b5eac1887a11bd6b72b170dbf286fc
Justin Sixsmith <justinsixsmith@gmail.com>
2022-11-02 14:56:21 -0500

    sets up BitSet class

---
+++ src/javaPrep/BitSet.java
@@ -0,0 +1,15 @@
+package javaPrep;
+
+import java.io.*;
+import java.util.*;
+
+public class BitSet {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+
+
+        sc.close();
+    }
+}
