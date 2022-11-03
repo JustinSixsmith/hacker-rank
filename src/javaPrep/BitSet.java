dae0e8b879f60800a8779dae6370617ad56cc875
Justin Sixsmith <justinsixsmith@gmail.com>
2022-11-03 10:52:26 -0500

    splits input into array of bit length and number of operations

--- src/javaPrep/BitSet.java
+++ src/javaPrep/BitSet.java
@@ -2,13 +2,15 @@ package javaPrep;
 
 import java.io.*;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class BitSet {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
-
+        final var lengthAndOps = Stream.of(sc.nextLine().split(" "));
+        System.out.println(Arrays.toString(lengthAndOps.toArray()));
 
         sc.close();
     }
