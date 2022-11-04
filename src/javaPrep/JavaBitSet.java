4c76e308dcff0cdb4986d6a3e87219ef75b9dc39
Justin Sixsmith <justinsixsmith@gmail.com>
2022-11-04 17:13:36 -0500

    returns string format

--- src/javaPrep/JavaBitSet.java
+++ src/javaPrep/JavaBitSet.java
@@ -1,6 +1,7 @@
 package javaPrep;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -35,15 +36,23 @@ public class JavaBitSet {
                     int i1 = Integer.parseInt(input[1]) - 1;
                     int i2 = Integer.parseInt(input[2]);
                     switch (Operation.valueOf(input[0])) {
-                        case AND: bitsets[i1].and(bitsets[i2 - 1]);
-                        case OR: bitsets[i1].or(bitsets[i2 - 1]);
-                        case XOR: bitsets[i1].xor(bitsets[i2 - 1]);
-                        case FLIP: bitsets[i1].flip(i2);
-                        case SET: bitsets[i1].set(i2);
+                        case AND:
+                            bitsets[i1].and(bitsets[i2 - 1]);
+                        case OR:
+                            bitsets[i1].or(bitsets[i2 - 1]);
+                        case XOR:
+                            bitsets[i1].xor(bitsets[i2 - 1]);
+                        case FLIP:
+                            bitsets[i1].flip(i2);
+                        case SET:
+                            bitsets[i1].set(i2);
                     }
-                });
+                    return String.format("%s %s", bitsets[0].cardinality(), bitsets[1].cardinality());
+                })
+                .collect(Collectors.joining("\n"));
 
-
+        System.out.println(output);
         sc.close();
     }
 }
+
