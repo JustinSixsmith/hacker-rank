6706dbe911828ab30f8bdfbff8101a425fbafe2d
Justin Sixsmith <justinsixsmith@gmail.com>
2022-11-10 22:29:52 -0600

    overrides divisor_sum method to find sum of divisors

--- src/javaPrep/JavaInterface.java
+++ src/javaPrep/JavaInterface.java
@@ -1,16 +1,29 @@
 package javaPrep;
 
 import java.util.*;
-interface AdvancedArithmetic{
+
+interface AdvancedArithmetic {
     int divisor_sum(int n);
 }
 
 //Write your code here
+class MyCalculator implements AdvancedArithmetic {
 
+    @Override
+    public int divisor_sum(int n) {
+        int sum = n;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
 
 
 class JavaInterface {
-    public static void main(String []args){
+    public static void main(String[] args) {
         MyCalculator my_calculator = new MyCalculator();
         System.out.print("I implemented: ");
         ImplementedInterfaceNames(my_calculator);
@@ -19,12 +32,13 @@ class JavaInterface {
         System.out.print(my_calculator.divisor_sum(n) + "\n");
         sc.close();
     }
+
     /*
      *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
      */
-    static void ImplementedInterfaceNames(Object o){
+    static void ImplementedInterfaceNames(Object o) {
         Class[] theInterfaces = o.getClass().getInterfaces();
-        for (int i = 0; i < theInterfaces.length; i++){
+        for (int i = 0; i < theInterfaces.length; i++) {
             String interfaceName = theInterfaces[i].getName();
             System.out.println(interfaceName);
         }
