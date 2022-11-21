2cf71455909860214aef5b9ee2de537a9ae6d7e4
Justin Sixsmith <justinsixsmith@gmail.com>
2022-11-20 23:28:04 -0600

    uses power of 2 method inside inner and private classes

--- src/javaPrep/Access.java
+++ src/javaPrep/Access.java
@@ -7,19 +7,19 @@ import java.util.regex.*;
 import java.security.*;
 
 
-public class Solution {
+public class Access {
 
     public static void main(String[] args) throws Exception {
         DoNotTerminate.forbidExit();
 
-        try{
+        try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             int num = Integer.parseInt(br.readLine().trim());
-            Object o;// Must be used to hold the reference of the instance of the class Solution.Inner.Private
+            Inner.Private o;// Must be used to hold the reference of the instance of the class Solution.Inner.Private
 
             //Write your code here
-
-
+            o = new Inner().new Private();
+            System.out.println(num + " is " + o.powerof2(num));
             System.out.println("An instance of class: " + o.getClass().getCanonicalName() + " has been created");
 
         }//end of try
@@ -28,10 +28,11 @@ public class Solution {
             System.out.println("Unsuccessful Termination!!");
         }
     }//end of main
-    static class Inner{
-        private class Private{
-            private String powerof2(int num){
-                return ((num&num-1)==0)?"power of 2":"not a power of 2";
+
+    static class Inner {
+        private class Private {
+            private String powerof2(int num) {
+                return ((num & num - 1) == 0) ? "power of 2" : "not a power of 2";
             }
         }
     }//end of Inner
