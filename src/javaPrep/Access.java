2493cc40c7d05338dd8f37ae8a303093729cb7df
Justin Sixsmith <justinsixsmith@gmail.com>
2022-11-20 23:19:10 -0600

    adds the rest of the access class

--- src/javaPrep/Access.java
+++ src/javaPrep/Access.java
@@ -1,7 +1,5 @@
 package javaPrep;
 
-import introduction.DoNotTerminate;
-
 import java.io.*;
 import java.lang.reflect.*;
 import java.util.*;
@@ -9,7 +7,7 @@ import java.util.regex.*;
 import java.security.*;
 
 
-public class Access {
+public class Solution {
 
     public static void main(String[] args) throws Exception {
         DoNotTerminate.forbidExit();
@@ -21,4 +19,42 @@ public class Access {
 
             //Write your code here
 
+
             System.out.println("An instance of class: " + o.getClass().getCanonicalName() + " has been created");
+
+        }//end of try
+
+        catch (DoNotTerminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }//end of main
+    static class Inner{
+        private class Private{
+            private String powerof2(int num){
+                return ((num&num-1)==0)?"power of 2":"not a power of 2";
+            }
+        }
+    }//end of Inner
+
+}//end of Solution
+
+class DoNotTerminate { //This class prevents exit(0)
+
+    public static class ExitTrappedException extends SecurityException {
+
+        private static final long serialVersionUID = 1L;
+    }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+}
+
