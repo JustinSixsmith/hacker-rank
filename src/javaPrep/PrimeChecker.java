f797c890f60875148986939cd5c169c889374e56
Justin Sixsmith <justinsixsmith@gmail.com>
2022-11-22 10:17:53 -0600

    creates prime class and checkPime method

---
+++ src/javaPrep/PrimeChecker.java
@@ -0,0 +1,17 @@
+package javaPrep;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class PrimeChecker {
+
+    class Prime {
+        public void checkPrime(BigInteger num) {
+        }
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
