f977d9a4c8a9dc64baaa5cbba188149a2f551bd0
Justin Sixsmith <justinsixsmith@gmail.com>
2022-11-22 10:27:56 -0600

    first attempt at checkPrime

--- src/javaPrep/PrimeChecker.java
+++ src/javaPrep/PrimeChecker.java
@@ -6,12 +6,23 @@ import java.util.*;
 
 public class PrimeChecker {
 
-    class Prime {
-        public void checkPrime(BigInteger num) {
+    static class Prime {
+        public static void checkPrime(int num) {
+            for (int i = 0; i < num; i++) {
+                BigInteger bi = BigInteger.valueOf(i);
+                if(bi.isProbablePrime(1)) {
+                    System.out.println(bi);
+                }
+            }
         }
     }
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            int num = sc.nextInt();
+            PrimeChecker.Prime.checkPrime(num);
+        }
     }
 }
