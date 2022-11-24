63d6761a2c8ba6231cfb60ed69ab812cd3887915
Justin Sixsmith <justinsixsmith@gmail.com>
2022-11-24 07:05:40 -0600

    creates simple method for x / y

---
+++ src/javaPrep/easy/ExceptionHandling.java
@@ -0,0 +1,23 @@
+package javaPrep.easy;
+
+import java.util.Scanner;
+
+public class ExceptionHandling {
+
+    static void divide(int x, int y) {
+        try {
+            System.out.println(x / y);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        ExceptionHandling.divide(x, y);
+    }
+}
