c90c750341826e8b708085ec37b1c354d145d6e5
Justin Sixsmith <justinsixsmith@gmail.com>
2022-11-25 18:11:29 -0600

    clears two of 5 tests

--- src/javaPrep/easy/ExceptionHandling.java
+++ src/javaPrep/easy/ExceptionHandling.java
@@ -5,21 +5,19 @@ import java.util.Scanner;
 
 public class ExceptionHandling {
 
-    static void divide(int x, int y) {
-        try {
-            System.out.println(x / y);
-        } catch (Exception e) {
-            System.out.println(e.getClass().getName());
-        }
-    }
-
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
 
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         int y = sc.nextInt();
-        ExceptionHandling.divide(x, y);
+        try {
+            System.out.println(x / y);
+        } catch (InputMismatchException e) {
+            System.out.println(e.getClass().getName());
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
         sc.close();
     }
 }
