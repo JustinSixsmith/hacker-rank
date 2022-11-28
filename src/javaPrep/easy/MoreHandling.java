b44468a889435f2ee107ef461c74a7cf10a4b05f
Justin Sixsmith <justinsixsmith@gmail.com>
2022-11-28 09:51:19 -0600

    adds a period at the end of the exception statements

--- src/javaPrep/easy/MoreHandling.java
+++ src/javaPrep/easy/MoreHandling.java
@@ -8,10 +8,10 @@ class MyCalculator {
      */
     long power(int x, int y) throws Exception {
         if (x < 0 || y < 0) {
-            throw new Exception("n or p should not be negative");
+            throw new Exception("n or p should not be negative.");
         }
         if (x == 0 && y == 0) {
-            throw new Exception("n and p should not be zero");
+            throw new Exception("n and p should not be zero.");
         }
         return (long) Math.pow(x, y);
     }
