4e8a38294562e303ef7af1e998d57dddbbedfbda
Justin Sixsmith <justinsixsmith@gmail.com>
2022-12-06 09:24:41 -0600

    creates medium package and content extractor class

---
+++ src/javaPrep/medium/ContentExtractor.java
@@ -0,0 +1,23 @@
+package javaPrep.medium;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class ContentExtractor {
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String line = in.nextLine();
+
+            //Write your code here
+
+            testCases--;
+        }
+    }
+}
