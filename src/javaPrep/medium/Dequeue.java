6f09d2cd1dc5a8b20e33240509d488633e2030c9
Justin Sixsmith <justinsixsmith@gmail.com>
2022-12-08 12:39:52 -0600

    solves for dequeue

--- src/javaPrep/medium/Dequeue.java
+++ src/javaPrep/medium/Dequeue.java
@@ -5,17 +5,33 @@ import java.util.*;
 public class Dequeue {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Deque<Object> deque = new ArrayDeque<>();
+        Deque<Integer> deque = new ArrayDeque<>();
+        HashMap<Integer, Integer> hashmap = new HashMap<>();
         int n = in.nextInt();
         int m = in.nextInt();
+        int ans = 0, distinct = 0;
 
         for (int i = 0; i < n; i++) {
             int num = in.nextInt();
-            if (!deque.contains(num)) {
-                deque.add(num);
+            deque.addLast(num);
+            hashmap.putIfAbsent(num, 0);
+
+            hashmap.put(num, hashmap.get(num) + 1);
+
+            if (hashmap.get(num) == 1) distinct++;
+
+            if (deque.size() == m + 1) {
+                int ele = deque.removeFirst();
+                hashmap.put(ele, hashmap.get(ele) - 1);
+                if (hashmap.get(ele) == 0) distinct--;
             }
+            if (deque.size() == m) {
+                if (distinct > ans) ans = distinct;
+            }
+
         }
-        System.out.println(deque.size());
+        System.out.println(ans);
+        in.close();
     }
 }
 
