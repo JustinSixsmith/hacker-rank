1a42c3d82b1c42dab4553e6e106e17d11593d8c0
Justin Sixsmith <justinsixsmith@gmail.com>
2022-12-09 10:23:14 -0600

    imports lombok methods

--- src/javaPrep/medium/PriorityQueue.java
+++ src/javaPrep/medium/PriorityQueue.java
@@ -6,8 +6,47 @@ import java.util.Scanner;
 /*
  * Create the Student and Priorities classes here.
  */
+
 class Student {
     private String name;
+
+    public Student() {
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Student)) return false;
+        final Student other = (Student) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$name = this.getName();
+        final Object other$name = other.getName();
+        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Student;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $name = this.getName();
+        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Student(name=" + this.getName() + ")";
+    }
 }
 
 class Priorities {
