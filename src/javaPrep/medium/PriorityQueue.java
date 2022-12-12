0eb00e9d5fdd10147cf21af3df9aa69df4cfc6ec
Justin Sixsmith <justinsixsmith@gmail.com>
2022-12-12 11:06:32 -0600

    returns student object

--- src/javaPrep/medium/PriorityQueue.java
+++ src/javaPrep/medium/PriorityQueue.java
@@ -1,15 +1,13 @@
 package javaPrep.medium;
 
-import lombok.Data;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 /*
  * Create the Student and Priorities classes here.
  */
 
-@Data
 class Student {
     private String name;
     private double CGPA;
@@ -20,21 +18,77 @@ class Student {
         this.CGPA = cgpa;
         this.id = id;
     }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getCGPA() {
+        return this.CGPA;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCGPA(double CGPA) {
+        this.CGPA = CGPA;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Student)) return false;
+        final Student other = (Student) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$name = this.getName();
+        final Object other$name = other.getName();
+        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
+        if (Double.compare(this.getCGPA(), other.getCGPA()) != 0) return false;
+        if (this.getId() != other.getId()) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Student;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $name = this.getName();
+        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
+        final long $CGPA = Double.doubleToLongBits(this.getCGPA());
+        result = result * PRIME + (int) ($CGPA >>> 32 ^ $CGPA);
+        result = result * PRIME + this.getId();
+        return result;
+    }
+
+    public String toString() {
+        return "Student(name=" + this.getName() + ", CGPA=" + this.getCGPA() + ", id=" + this.getId() + ")";
+    }
 }
 
 class Priorities {
 
     public List<Student> getStudents(List<String> events) {
-        ArrayList<String> students = new ArrayList<>();
+        List<Student> students = new ArrayList<>();
         for (String st : events) {
-            System.out.println(st);
             String[] stStud = st.split(" ");
-            String name = stStud[0];
-            double CGPA = Double.parseDouble(stStud[1]);
-            int id = Integer.parseInt(stStud[2]);
-            students.add(new Student(name, CGPA, id);
+            String name = stStud[1];
+            double CGPA = Double.parseDouble(stStud[2]);
+            int id = Integer.parseInt(stStud[3]);
+            Student tempStu = new Student(name, CGPA, id);
+            students.add(tempStu);
         }
-        return null;
+        return students;
     }
 }
 
@@ -57,7 +111,7 @@ public class PriorityQueue {
             System.out.println("EMPTY");
         } else {
             for (Student st: students) {
-//                System.out.println(st.getName());
+                System.out.println(st.getName());
             }
         }
     }
