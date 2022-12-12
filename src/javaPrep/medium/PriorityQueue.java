3845fb199b918cb3346a48a8b6962ddb3b755b73
Justin Sixsmith <justinsixsmith@gmail.com>
2022-12-12 09:00:25 -0600

    constructs student from string input

--- src/javaPrep/medium/PriorityQueue.java
+++ src/javaPrep/medium/PriorityQueue.java
@@ -14,15 +14,25 @@ class Student {
     private String name;
     private double CGPA;
     private int id;
+
+    public Student(String name, double cgpa, int id) {
+        this.name = name;
+        this.CGPA = cgpa;
+        this.id = id;
+    }
 }
 
 class Priorities {
 
     public List<Student> getStudents(List<String> events) {
-        ArrayList<String> roster = new ArrayList<>();
+        ArrayList<String> students = new ArrayList<>();
         for (String st : events) {
             System.out.println(st);
-            String[] StStud = st.split(" ");
+            String[] stStud = st.split(" ");
+            String name = stStud[0];
+            double CGPA = Double.parseDouble(stStud[1]);
+            int id = Integer.parseInt(stStud[2]);
+            students.add(new Student(name, CGPA, id);
         }
         return null;
     }
