179f0ddcf07104efc6bbf0fef0c5e3f0e55a2743
Justin Sixsmith <justinsixsmith@gmail.com>
2022-12-15 21:10:38 -0600

    simplifies student class

--- src/javaPrep/medium/PriorityQueue.java
+++ src/javaPrep/medium/PriorityQueue.java
@@ -6,70 +6,24 @@ import java.util.*;
  */
 
 class Student {
-    private String name;
-    private double CGPA;
-    private int id;
+    int id;
+    String name;
+    double cgpa;
 
-    public Student(String name, double cgpa, int id) {
-        this.name = name;
-        this.CGPA = cgpa;
+    public Student(int id, String name, double cgpa) {
         this.id = id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public double getCGPA() {
-        return this.CGPA;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public void setName(String name) {
         this.name = name;
+        this.cgpa = cgpa;
     }
 
-    public void setCGPA(double CGPA) {
-        this.CGPA = CGPA;
+    public int getID(){
+        return id;
     }
-
-    public void setId(int id) {
-        this.id = id;
+    public String getName(){
+        return name;
     }
-
-    public boolean equals(final Object o) {
-        if (o == this) return true;
-        if (!(o instanceof Student)) return false;
-        final Student other = (Student) o;
-        if (!other.canEqual((Object) this)) return false;
-        final Object this$name = this.getName();
-        final Object other$name = other.getName();
-        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
-        if (Double.compare(this.getCGPA(), other.getCGPA()) != 0) return false;
-        if (this.getId() != other.getId()) return false;
-        return true;
-    }
-
-    protected boolean canEqual(final Object other) {
-        return other instanceof Student;
-    }
-
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final Object $name = this.getName();
-        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
-        final long $CGPA = Double.doubleToLongBits(this.getCGPA());
-        result = result * PRIME + (int) ($CGPA >>> 32 ^ $CGPA);
-        result = result * PRIME + this.getId();
-        return result;
-    }
-
-    public String toString() {
-        return "Student(name=" + this.getName() + ", CGPA=" + this.getCGPA() + ", id=" + this.getId() + ")";
+    public double getCGPA(){
+        return cgpa;
     }
 }
 
