5aa935fdf5571af29c79cc3adf99a0d87853dc96
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-03-15 08:39:04 -0500

    Refactors Pivot Index

--- out/production/hacker-rank/leet-code-js/longest-common-prefix.js
+++ out/production/hacker-rank/leet-code-js/longest-common-prefix.js
@@ -28,22 +28,41 @@
 // strings = ["dog", "racecar", "car"];
 // console.log(longestCommonPrefix(strings)); // Output: ""
 
+// function pivotIndex(nums) {
+//   let leftSum = 0;
+//   let rightSum = 0;
+//   let pivot = -1;
+//   for (let i = 0; i < nums.length; i++) {
+//     let pivotIndex = i + 1;
+//     leftSum += nums[i];
+//     for (let j = nums.length - 1; j > nums[pivotIndex]; j--) {
+//       rightSum += nums[j];
+//     }
+//     if (leftSum === rightSum) {
+//       pivot = pivotIndex;
+//       break;
+//     }
+//   }
+//   return pivot;
+// }
+
+// Right sum equals total of array to start
 function pivotIndex(nums) {
+  let rightSum = nums.reduce((a, b) => a + b, 0);
   let leftSum = 0;
-  let rightSum = 0;
-  let pivot = -1;
+
+  // Loop over array, subtracting the num at index i to find new right sum
   for (let i = 0; i < nums.length; i++) {
-    let pivotIndex = i + 1;
+    rightSum -= nums[i];
+
+    // Compare with left sum, and if equal, return i
+    if (leftSum === rightSum) return i;
+
+    // If not move left sum up an index and add
     leftSum += nums[i];
-    for (let j = nums.length - 1; j > nums[pivotIndex]; j--) {
-      rightSum += nums[j];
-    }
-    if (leftSum === rightSum) {
-      pivot = pivotIndex;
-      break;
-    }
   }
-  return pivot;
+
+  return -1;
 }
 
 const nums = [1, 7, 3, 6, 5, 6];
---
+++ out/production/hacker-rank/leet-code-js/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+const nums = [1, 7, 3, 6, 5, 6];
+console.log(pivotIndex(nums));
