0fc440b26b02c00d6cb27d5e4105edcf06e1c362
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-03-19 23:36:05 -0500

    Finds max profit with one pass

--- out/production/hacker-rank/leet-code-js/buy-and-sell.js
+++ out/production/hacker-rank/leet-code-js/buy-and-sell.js
@@ -1,17 +1,31 @@
-function maxProfit(prices) {
-  // Iterate through the array
-  let maxProfit = 0;
-  for (let i = 1; i < prices.length; i++) {
-    const todaysPrice = prices[i];
+// function maxProfit(prices) {
+//   // Iterate through the array
+//   let maxProfit = 0;
+//   for (let i = 1; i < prices.length; i++) {
+//     const todaysPrice = prices[i];
 
-    // Look at the future prices
-    for (let j = i - 1; j >= 0; j--) {
-      const futurePrice = prices[j];
-      // if the future price minus the current price is greater than the max profit
-      const profit = todaysPrice - futurePrice;
-      if (profit > maxProfit) {
-        maxProfit = profit;
-      }
+//     // Look at the future prices
+//     for (let j = i - 1; j >= 0; j--) {
+//       const futurePrice = prices[j];
+//       // if the future price minus the current price is greater than the max profit
+//       const profit = todaysPrice - futurePrice;
+//       if (profit > maxProfit) {
+//         maxProfit = profit;
+//       }
+//     }
+//   }
+//   return maxProfit;
+// }
+
+function maxProfit(prices) {
+  let minPrice = Number.MAX_VALUE;
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    } else if (prices[i] - minPrice > maxProfit) {
+      maxProfit = prices[i] - minPrice;
     }
   }
   return maxProfit;
