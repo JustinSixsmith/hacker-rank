c7d0ad6459c8dbba54b8d7c8c27ae26e7c8ffc81
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-03-23 09:02:32 -0500

    Refactors to a for of loop

--- out/production/hacker-rank/leet-code-js/buy-and-sell.js
+++ out/production/hacker-rank/leet-code-js/buy-and-sell.js
@@ -1,31 +1,12 @@
-// function maxProfit(prices) {
-//   // Iterate through the array
-//   let maxProfit = 0;
-//   for (let i = 1; i < prices.length; i++) {
-//     const todaysPrice = prices[i];
-
-//     // Look at the future prices
-//     for (let j = i - 1; j >= 0; j--) {
-//       const futurePrice = prices[j];
-//       // if the future price minus the current price is greater than the max profit
-//       const profit = todaysPrice - futurePrice;
-//       if (profit > maxProfit) {
-//         maxProfit = profit;
-//       }
-//     }
-//   }
-//   return maxProfit;
-// }
-
 function maxProfit(prices) {
   let minPrice = Number.MAX_VALUE;
   let maxProfit = 0;
 
-  for (let i = 0; i < prices.length; i++) {
-    if (prices[i] < minPrice) {
-      minPrice = prices[i];
-    } else if (prices[i] - minPrice > maxProfit) {
-      maxProfit = prices[i] - minPrice;
+  for (const price of prices) {
+    if (price < minPrice) {
+      minPrice = price;
+    } else if (price - minPrice > maxProfit) {
+      maxProfit = price - minPrice;
     }
   }
   return maxProfit;
