2cbe85c0a1d3490893ef5257fdb2a7bd64c3357d
Justin Sixsmith <justinsixsmith@gmail.com>
2023-07-19 13:18:13 -0500

    Counts swaps in bubble sort

--- src/interviewPrep/ArrayUtils.java
+++ src/interviewPrep/ArrayUtils.java
@@ -1,9 +1,7 @@
 package interviewPrep;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.sql.Array;
+import java.util.*;
 
 public class ArrayUtils {
     public static int hourglassSum(List<List<Integer>> arr) {
@@ -210,6 +208,31 @@ public class ArrayUtils {
         }
 
         return count;
+    }
+
+    public static void countSwaps(List<Integer> a) {
+        int n = a.size();
+        int numSwaps = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a.get(j) > a.get(j + 1)) {
+                    swap(a, j, j + 1);
+                    numSwaps++;
+                }
+            }
+        }
+
+        System.out.println("Array is sorted in " + numSwaps + " swaps.");
+        System.out.println("First Element: " + a.get(0));
+        System.out.println("Last Element: " + a.get(n - 1));
+    }
+
+    private static void swap(List<Integer> a, int i, int j) {
+        int temp = a.get(i);
+        a.set(i, a.get(j));
+        a.set(j, temp);
     }
 }
 
