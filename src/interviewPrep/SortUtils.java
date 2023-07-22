4d91117906a9d840bafb378a4fd377ba358699d4
Justin Sixsmith <justinsixsmith@gmail.com>
2023-07-21 19:12:53 -0500

    Bubble sorts

--- src/interviewPrep/SortUtils.java
+++ src/interviewPrep/SortUtils.java
@@ -44,4 +44,19 @@ public class SortUtils {
 
         return toys;
     }
+
+    public static long countInversions(List<Integer> arr) {
+        int count = 0;
+        // Write your code here
+        for (int i = 0; i < arr.size() - 1; i++) {
+            for (int j = i + 1; j < arr.size(); j++) {
+                if (arr.get(i) > arr.get(j)) {
+                    swap(arr, i, j);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
 }
