233d4c738f27e9a81774e151df612b2776f07fb6
Justin Sixsmith <justinsixsmith@gmail.com>
2023-08-09 16:23:15 -0500

    Finds a solution for common child

--- src/interviewPrep/StringUtils.java
+++ src/interviewPrep/StringUtils.java
@@ -193,5 +193,25 @@ public class StringUtils {
         return count;
     }
 
+    public static int commonChild(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        int[][] lcs = new int[n + 1][m + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0 || j == 0) {
+                    lcs[i][j] = 0;
+                } else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    lcs[i][j] = lcs[i - 1][j - 1] + 1;
+                } else {
+                    lcs[i][j] = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
+                }
+            }
+        }
+
+        return lcs[n][m];
+    }
 
 }
