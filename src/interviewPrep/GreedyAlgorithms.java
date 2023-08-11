dc20f3acdeadf76b720b4103241a050b5c8feb5d
Justin Sixsmith <justinsixsmith@gmail.com>
2023-08-11 17:37:19 -0500

    Sorts array first for better efficiency

--- src/interviewPrep/GreedyAlgorithms.java
+++ src/interviewPrep/GreedyAlgorithms.java
@@ -1,19 +1,19 @@
 package interviewPrep;
 
+import java.util.Collections;
 import java.util.List;
 
 public class GreedyAlgorithms {
 
     public static int minimumAbsoluteDifference(List<Integer> arr) {
         // Write your code here
+        Collections.sort(arr);
         int minAbs = Integer.MAX_VALUE;
 
         for (int i = 0; i < arr.size() - 1; i++) {
-            for (int j = i + 1; j < arr.size(); j++) {
-                int abs = Math.abs(arr.get(i) - arr.get(j));
-                if (abs < minAbs)
-                    minAbs = abs;
-            }
+            int abs = Math.abs(arr.get(i) - arr.get(i + 1));
+            if (abs < minAbs)
+                minAbs = abs;
         }
 
         return minAbs;
