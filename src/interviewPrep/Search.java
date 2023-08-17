4456a0a523bf7c40c37e0e2eac1d387231a0d0cb
Justin Sixsmith <justinsixsmith@gmail.com>
2023-08-17 18:45:14 -0500

    Simplifies solution to fit problem

--- src/interviewPrep/Search.java
+++ src/interviewPrep/Search.java
@@ -5,30 +5,15 @@ import java.util.List;
 public class Search {
 
     public static void whatFlavors(List<Integer> cost, int money) {
-        // Write your code here
-        int sunny = 0;
-        int johnny = 0;
-        int closest = Integer.MAX_VALUE;
-
         for (int i = 0; i < cost.size() - 1; i++) {
-            for (int j = 1; j < cost.size(); j++) {
-                int first = cost.get(i);
-                int second = cost.get(j);
-                int difference = money - (first + second);
-
-                if (difference >= 0 && difference < money && difference < closest) {
-                    closest = difference;
-                    if (first < second) {
-                        sunny = i + 1;
-                        johnny = j + 1;
-                    } else {
-                        johnny = j + 1;
-                        sunny = i + 1;
-                    }
+            for (int j = i + 1; j < cost.size(); j++) {
+                if (cost.get(i) + cost.get(j) == money) {
+                    // Ensure smaller index is printed first
+                    System.out.println((i + 1) + " " + (j + 1));
+                    return;  // exit early once we find a match
                 }
             }
         }
-
-        System.out.println(sunny + " " + johnny);
     }
+
 }
