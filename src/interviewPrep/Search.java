ff76ca5b9af8ddee009af98f03b9c6e6ea125483
Justin Sixsmith <justinsixsmith@gmail.com>
2023-08-17 18:36:01 -0500

    First pass ice cream parlor

---
+++ src/interviewPrep/Search.java
@@ -0,0 +1,34 @@
+package interviewPrep;
+
+import java.util.List;
+
+public class Search {
+
+    public static void whatFlavors(List<Integer> cost, int money) {
+        // Write your code here
+        int sunny = 0;
+        int johnny = 0;
+        int closest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < cost.size() - 1; i++) {
+            for (int j = 1; j < cost.size(); j++) {
+                int first = cost.get(i);
+                int second = cost.get(j);
+                int difference = money - (first + second);
+
+                if (difference >= 0 && difference < money && difference < closest) {
+                    closest = difference;
+                    if (first < second) {
+                        sunny = first;
+                        johnny = second;
+                    } else {
+                        johnny = first;
+                        sunny = second;
+                    }
+                }
+            }
+        }
+
+        System.out.println(sunny + " " + johnny);
+    }
+}
